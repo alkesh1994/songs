f849e859aa3d9a02e8bd1911453610ac8ce9ef24
alkeshkoyande <alkeshkoyande@gmail.com>
2020-08-26 21:30:36 +0530

    Updates

---
+++ precache-manifest.d25b505a2ac365d209db0327150ec773.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d1b7751f0220f6cf5477761aa2984b",
+    "url": "/songs/index.html"
+  },
+  {
+    "revision": "26852b63ef6a08de1aaa",
+    "url": "/songs/static/js/2.cb3504c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/songs/static/js/2.cb3504c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "487949aa6711a077e436",
+    "url": "/songs/static/js/main.96d74639.chunk.js"
+  },
+  {
+    "revision": "b8c7de70643b8ccc6673",
+    "url": "/songs/static/js/runtime-main.f9a15bfc.js"
+  }
+]);
